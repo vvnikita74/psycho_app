6c0e168e4915374bc845e1f832783fd08c610ea3
vvnikita74 <124995385+vvnikita74@users.noreply.github.com>
2025-04-03 13:03:59 +0300

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Config {
   static const Color backgroundColor = Color(0xFF9DE1FC);
+  static const Color headlineColor = Color(0xFFFFFFFF);
+
+  static TextStyle headlineStyle = GoogleFonts.rubik(
+    color: headlineColor,
+    height: 1.0,
+    fontWeight: FontWeight.w700,
+  );
 }
