8303267269a7fc759a800e882e34dcaf11355239
vvnikita74 <124995385+vvnikita74@users.noreply.github.com>
2025-04-07 05:53:16 +0300

    state and optimizations

--- lib/config.dart
+++ lib/config.dart
@@ -3,11 +3,35 @@ import 'package:google_fonts/google_fonts.dart';
 
 class Config {
   static const Color backgroundColor = Color(0xFF9DE1FC);
-  static const Color headlineColor = Color(0xFFFFFFFF);
+  static const Color headlineColor = Color(0xFFFCFCFE);
+  static const Color buttonTextColor = Color(0xFF1582B4);
+  static const Color backgroundLightColor = Color(0xD9C5EEFF);
+  static const double basePadding = 12.0;
+
+  static const EdgeInsets contentPadding = EdgeInsets.all(basePadding);
+
+  static ButtonStyle buttonStyle = ButtonStyle(
+    backgroundColor: WidgetStatePropertyAll<Color>(backgroundLightColor),
+    padding: WidgetStatePropertyAll<EdgeInsets>(EdgeInsets.all(12)),
+    shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
+      RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+        side: BorderSide(color: buttonTextColor),
+      ),
+    ),
+  );
 
   static TextStyle headlineStyle = GoogleFonts.rubik(
     color: headlineColor,
     height: 1.0,
     fontWeight: FontWeight.w700,
+    fontSize: 24,
+  );
+
+  static TextStyle buttonTextStyle = GoogleFonts.montserrat(
+    color: buttonTextColor,
+    height: 1.05,
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
   );
 }
--- lib/pages/auth/register.dart
+++ lib/pages/auth/register.dart
@@ -1,45 +1,186 @@
 import 'package:flutter/material.dart';
 import 'package:psycho_app/config.dart';
 
-const UnderlineInputBorder inputUnderline = UnderlineInputBorder(
-  borderSide: BorderSide(color: Config.headlineColor, width: 2.0),
-);
+// TODO: Вынести в отдельный файл или вообще убрать
+enum FieldType { text, radio }
 
-class RegisterPage extends StatelessWidget {
+class FormFieldConfig {
+  final String label;
+  final bool obscureText;
+  final TextInputType? keyboardType;
+  final String fieldKey;
+  final FieldType fieldType;
+
+  FormFieldConfig({
+    required this.label,
+    required this.fieldKey,
+    this.obscureText = false,
+    this.fieldType = FieldType.text,
+    this.keyboardType,
+  });
+}
+
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final List<FormFieldConfig> formFields = [
+    FormFieldConfig(label: 'Как к вам обращаться?', fieldKey: 'name'),
+    FormFieldConfig(label: 'Сколько вам лет?', fieldKey: 'age'),
+    FormFieldConfig(label: 'Кто вы?', fieldKey: 'sex'),
+  ];
+
+  int currentFieldIndex = 0;
+  final textController = TextEditingController();
+
+  void _nextField() {
+    if (currentFieldIndex < formFields.length) {
+      setState(() {
+        textController.clear();
+        currentFieldIndex++;
+      });
+    }
+  }
+
+  void _prevField() {
+    if (currentFieldIndex != 0) {
+      setState(() {
+        textController.clear();
+        currentFieldIndex--;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: Config.contentPadding,
+          child: Stack(
+            children: [
+              RegisterField(
+                label: formFields[currentFieldIndex].label,
+                controller: textController,
+              ),
+              if (currentFieldIndex > 0)
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  child: IconButton.filled(
+                    style: const ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll<Color>(
+                        Config.backgroundLightColor,
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(Config.basePadding / 2),
+                    constraints: const BoxConstraints(),
+                    icon: const Icon(
+                      Icons.chevron_left,
+                      size: 24,
+                      color: Config.buttonTextColor,
+                    ),
+                    onPressed: _prevField,
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          alignment: FractionalOffset.center,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  style: Config.buttonStyle,
+                  onPressed: _nextField,
+                  child: Text('Продолжить', style: Config.buttonTextStyle),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(
+                  bottom: (Config.basePadding * 2),
+                  top: 12,
+                ),
+                child: Text(
+                  'У меня есть аккаунт',
+                  style: Config.buttonTextStyle.copyWith(
+                    color: Config.headlineColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterField extends StatelessWidget {
+  const RegisterField({
+    super.key,
+    required this.label,
+    this.controller,
+    this.obscureText = false,
+    this.keyboardType,
+  });
+
+  final TextEditingController? controller;
+  final String label;
+  final bool obscureText;
+  final TextInputType? keyboardType;
 
   @override
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
+    const inputUnderline = UnderlineInputBorder(
+      borderSide: BorderSide(color: Config.headlineColor, width: 2.0),
+    );
 
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Как к вам\nобращаться?',
-              textAlign: TextAlign.center,
-              style: textTheme.headlineMedium,
-            ),
-            Center(
-              child: Container(
-                constraints: BoxConstraints(maxWidth: 500),
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: TextField(
-                  style: textTheme.headlineSmall,
-                  textAlign: TextAlign.center,
-                  cursorColor: Config.headlineColor,
-                  decoration: InputDecoration(
-                    enabledBorder: inputUnderline,
-                    focusedBorder: inputUnderline,
-                  ),
-                ),
-              ),
-            ),
-          ],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: double.infinity,
+          child: Text(
+            label,
+            textAlign: TextAlign.center,
+            style: textTheme.headlineMedium,
+          ),
         ),
-      ),
+        FractionallySizedBox(
+          widthFactor: 0.4,
+          alignment: FractionalOffset.center,
+          child: TextField(
+            controller: controller,
+            style: textTheme.headlineSmall,
+            textAlign: TextAlign.center,
+            keyboardType: keyboardType,
+            obscureText: obscureText,
+            cursorColor: Config.headlineColor,
+            decoration: InputDecoration(
+              enabledBorder: inputUnderline,
+              focusedBorder: inputUnderline,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
